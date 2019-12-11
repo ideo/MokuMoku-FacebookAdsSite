df52cc0e50459eb2484a5790b4814aef144fa95f
Takashi Wickes <twickes@ideo.com>
2019-12-11 11:05:55 -0800

    Added modal interaction

---
+++ js/modal.js
@@ -0,0 +1,23 @@
+var modal = document.getElementById("alertModal");
+var button = document. getElementById("signUpButton");
+var close = document.getElementById("closeButton");
+
+button.onclick = function() {
+    modal.classList.add("modal-inView");
+    // modal.style.display = "block";
+    console.log("modal");
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  close.onclick = function() {
+    // modal.style.display = "none";
+        modal.classList.remove("modal-inView");
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+    //   modal.style.display = "none";
+        modal.classList.remove("modal-inView");
+    }
+  }
